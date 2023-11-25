e79ac93b15a89f9b4fdf9b281c6c761aa156df8a
hiro <hirohanahaba@gmail.com>
2023-11-26 00:29:59 +0900

    @importのために、tsconfig.jsonとvite.config.tsファイルの修正完了。

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import ProductList from "./components/ProductList.vue";
-import ShoppingCart from "./components/ShoppingCart.vue";
+import ProductList from "@/components/ProductList.vue";
+import ShoppingCart from "@/components/ShoppingCart.vue";
 </script>
 
 <template>
